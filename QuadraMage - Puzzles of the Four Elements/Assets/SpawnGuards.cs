b305d6bff403885b9a5aeede8c9ca31e75d9d725
Marcel Felber <marcel.felber.mail@gmail.com>
2024-04-09 17:44:53 +0200

    Devil Palace - version 0.7.6 🎉

---
+++ QuadraMage - Puzzles of the Four Elements/Assets/SpawnGuards.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGuards : MonoBehaviour
+{
+    GameObject guard1;
+    GameObject guard2;
+    GameObject guard3;
+    GameObject guard4;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
