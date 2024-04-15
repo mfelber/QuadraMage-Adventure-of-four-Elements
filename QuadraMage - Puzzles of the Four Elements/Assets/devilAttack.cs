2991b937d1ece74301ef08d9c0c6e3d7dcb3c53b
Marcel Felber <marcel.felber.mail@gmail.com>
2024-04-15 20:32:45 +0200

    test

--- QuadraMage - Puzzles of the Four Elements/Assets/devilAttack.cs
+++ QuadraMage - Puzzles of the Four Elements/Assets/devilAttack.cs
@@ -6,41 +6,49 @@ public class devilAttack : MonoBehaviour
 {
     private GameObject player;
     private Rigidbody2D rb;
+    public Rigidbody2D playerRB;
     public float force;
     public PlayerMovement PlayerMovement;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        
         player = GameObject.FindGameObjectWithTag("Player");
 
         Vector3 direction = player.transform.position - transform.position;
         rb.velocity = new Vector2(direction.x,direction.y).normalized * force;
         Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Shot"), LayerMask.NameToLayer("Ground"), true);
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Shot"), LayerMask.NameToLayer("Shot"), true);
+        Physics2D.IgnoreLayerCollision(LayerMask.NameToLayer("Shot"), LayerMask.NameToLayer("FirstAttack"), true);
     }
 
     // Update is called once per frame
+    public bool hitplayer;
     void Update()
     {
-        
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
+        /*
+        if(hitplayer)
         {
-            //Debug.Log("kolizia");
-            PlayerMovement.HowMuchTimeIsLeft = PlayerMovement.TimeOfKnockBack;
-            if (collision.transform.position.x <= transform.position.x)
-            {
-                PlayerMovement.knockBackFromR = true;
+            player.transform.position = new Vector2(player.transform.position.x - 0.5f, player.transform.position.y + 0.5f);
+            hitplayer = false;
+        }
+        */
 
-            }
-            if (collision.transform.position.x > transform.position.x)
-            {
-                PlayerMovement.knockBackFromR = false;
-
-            }
+        if (hitplayer)
+        {
+            playerRB.velocity = new Vector2(-10, 10);
         }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            hitplayer = true;
+            Destroy(gameObject, 0.1f);
+        }
+    }
+
+    
 }
